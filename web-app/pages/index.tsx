d3205ba5eb18db85e1b028b59d54344714f6a728
_uchanlee <uchanlee.dev@gmail.com>
2021-03-18 16:19:19 +0900

    컴포넌트 작성법 통일

--- web-app/pages/index.tsx
+++ web-app/pages/index.tsx
@@ -5,21 +5,21 @@ import { ScrolllLayout } from 'layouts/ScrollLayout';
 import { ContentList } from '@components/content/content-list';
 import { Banner } from 'components/banner';
 
-const HomePage = () => (
-  <ScrolllLayout y={true}>
-    <Banner />
-    <div
-      css={css`
-        padding-left: 10px;
-        display: flex;
-        flex-direction: column;
-      `}
-    >
-      <ContentList />
-      <ContentList />
-      <ContentList />
-    </div>
-  </ScrolllLayout>
-);
-
-export default HomePage;
+export default function HomePage() {
+  return (
+    <ScrolllLayout y={true}>
+      <Banner />
+      <div
+        css={css`
+          padding-left: 10px;
+          display: flex;
+          flex-direction: column;
+        `}
+      >
+        <ContentList />
+        <ContentList />
+        <ContentList />
+      </div>
+    </ScrolllLayout>
+  );
+}
