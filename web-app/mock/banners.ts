8faf5f3eadf2cc6a1ceaec5d55e61ab87fbdd1d0
_uchanlee <uchanlee.dev@gmail.com>
2021-03-25 09:10:09 +0900

    목 데이터 추가

---
+++ web-app/mock/banners.ts
@@ -0,0 +1,15 @@
+export type Banner = {
+  id: number;
+  bannerUrl: string;
+};
+
+export const banners: Banner[] = [
+  {
+    id: 1,
+    bannerUrl: '/images/entryBanner.png',
+  },
+  {
+    id: 2,
+    bannerUrl: '/images/dsmBanner.png',
+  },
+];
