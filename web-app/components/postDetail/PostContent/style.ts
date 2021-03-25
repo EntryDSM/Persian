9d79afb5e36febb8f9e8d666cbea9bbe18daadb9
_uchanlee <uchanlee.dev@gmail.com>
2021-03-25 09:25:04 +0900

    ENTRYINIT-T-10 게시글 상세 UI 마크업

---
+++ web-app/components/postDetail/PostContent/style.ts
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+export const PostContentWrapper = styled.div`
+  padding: 20px;
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    margin-top: 10px;
+    margin-bottom: 8px;
+  }
+
+  > p {
+    margin-bottom: 14px;
+  }
+
+  li + li {
+    margin-top: 6px;
+  }
+
+  a {
+    color: blue;
+  }
+
+  img {
+    width: 100%;
+    margin: 8px 0;
+    height: auto;
+  }
+`;
