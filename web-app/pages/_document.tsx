375c40c04b5dca2cc1e8b3b2cf165933f5097633
_uchanlee <uchanlee.dev@gmail.com>
2021-04-14 10:16:29 +0900

    ENTRYINIT-T-11 PWA 설정

--- web-app/pages/_document.tsx
+++ web-app/pages/_document.tsx
@@ -1,6 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
+const APP_NAME = 'INIT';
+const APP_DESCRIPTION = '대덕소프트웨어마이스터고등학교 커뮤니티';
+
+export default class MyDocument extends Document {
   render() {
     return (
       <Html lang='ko'>
@@ -31,6 +34,26 @@ class MyDocument extends Document {
             type='text/css'
             href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css'
           />
+          <meta name='application-name' content={APP_NAME} />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta
+            name='apple-mobile-web-app-status-bar-style'
+            content='default'
+          />
+          <meta name='apple-mobile-web-app-title' content={APP_NAME} />
+          <meta name='description' content={APP_DESCRIPTION} />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='theme-color' content='red' />
+          {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
+          {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/icons/apple-touch-icon.png'
+          />
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='shortcut icon' href='/icons/favicon.ico' />
         </Head>
         <body>
           <Main />
@@ -40,5 +63,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
